ac64a5d9a27650eab15b8bb4c11b7db1eff6dc8b
bluemario8 <bluemario812@gmail.com>
2025-04-16 07:36:41 -0400

    Admin can now create more admin accounts (register page admin type added)

--- register.py
+++ register.py
@@ -7,6 +7,8 @@ register_bp = Blueprint("register", __name__, static_folder="static",
 
 @register_bp.route("/register", methods=["GET", "POST"])
 def register():
+    isAdmin = current_user.is_authenticated and current_user.type == 'admin'
+    print(f"isAdmin: {isAdmin}")
     if request.method == "POST":
         email = request.form.get('email')
         username = request.form.get('username')
@@ -19,15 +21,15 @@ def register():
         # Check if the values are not null (tampered with website)
         if not email or not username or not password \
             or not first_name or not last_name or not type:
-            return render_template("register.html", error="Error: Invalid input")
+            return render_template("register.html", error="Error: Invalid input", isAdmin=isAdmin)
         elif Users.query.filter_by(email=email).first():
-            return render_template("register.html", error="Error: Email already exists")
+            return render_template("register.html", error="Error: Email already exists", isAdmin=isAdmin)
         elif email.find('@') == -1 or email.find('.') == -1:
-            return render_template("register.html", error="Error: Invalid email")
+            return render_template("register.html", error="Error: Invalid email", isAdmin=isAdmin)
         elif Users.query.filter_by(username=username).first():
-            return render_template("register.html", error="Error: Username already exists")
+            return render_template("register.html", error="Error: Username already exists", isAdmin=isAdmin)
         elif password != repassword:
-            return render_template("register.html", error="Error: Passwords do not match")
+            return render_template("register.html", error="Error: Passwords do not match", isAdmin=isAdmin)
 
         hashed_pswd = bcrypt.generate_password_hash(request.form.get('password')).decode('utf-8')
         new_user = Users(email=email, username=username, hashed_pswd=hashed_pswd, 
@@ -35,6 +37,6 @@ def register():
         db.session.add(new_user)
         db.session.commit()
 
-        return render_template("register.html", success="Created account. <a href=\"/login\">Login</a>") 
+        return render_template("register.html", success="Created account. <a href=\"/login\">Login</a>", isAdmin=isAdmin) 
 
-    return render_template("register.html")
+    return render_template("register.html", isAdmin = isAdmin)
