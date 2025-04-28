23ef2d288ae5ee6443202818200b935003c7d853
EllieMott <elizabethmott1326@gmail.com>
2025-04-28 08:38:32 -0400

    removed commented out former code, fixed comment spacing

--- search/search.py
+++ search/search.py
@@ -5,10 +5,11 @@ from extensions import conn
 search_bp = Blueprint('search', __name__, static_folder='static_search', template_folder='templates_search')
 
 # -- SEARCH PAGE -- #
+@search_bp.route('/search', methods=['GET', 'POST'])
 @search_bp.route('/search/search.py', methods=['GET', 'POST'])
 def search():
-    # --- Fetch all product data at once ---
-    product_data = conn.execute(text('''
+    product_data = conn.execute(                                    # fetch all product data
+        text('''
         SELECT p.product_id, p.product_title, p.cat_num,
                MIN(v.price) AS min_price, MAX(v.price) AS max_price,
                u.username AS vendor_name
@@ -45,7 +46,6 @@ def search():
             'category': cat_num,
             'display': True
         })
-
 
     vendors = conn.execute(                                         # fetch vendors
         text('''                                 
@@ -136,184 +136,6 @@ def search():
         priceValue=1000,
         clearDisplay='none'
     )
-
-
-
-
-
-
-# @search_bp.route('/search/search.py', methods=['GET', 'POST'])
-# def search():
-#     productIDs = conn.execute(
-#         text('SELECT product_id FROM products;')).fetchall()
-#     productOptions = conn.execute(
-#         text(''' 
-#             SELECT product_id, COUNT(variant_id)
-#             FROM product_variants GROUP BY product_id
-#             ORDER BY product_id;
-#             ''')).fetchall()
-#     productPriceRange = []
-#     productVendor = conn.execute(
-#         text('''
-#             SELECT p.product_id, u.username
-#             FROM products p
-#             JOIN users u ON p.vendor_id = u.email
-#         ''')).fetchall()
-    
-#     for id_tuple in productIDs:
-#         id = id_tuple[0]
-
-#         productTS = conn.execute(
-#             text('''
-#                 SELECT product_id, product_title
-#                 FROM products
-#                 WHERE product_id IN :ids;
-#             '''),
-#         {'ids': tuple(pid[0] for pid in productIDs)}).fetchall()
-
-#         prodCG = conn.execute(
-#             text('''
-#                 SELECT product_id, cat_num FROM products 
-#                 WHERE product_id IN :ids;
-#             '''), 
-#             {'ids': tuple(pid[0] for pid in productIDs)}).fetchall()
-#         print('prodCG:', prodCG)
-
-#         prodPR = conn.execute(
-#             text('''
-#                 SELECT MIN(price), MAX(price)
-#                 FROM product_variants
-#                 WHERE product_id = :id;
-#                  '''), {'id': id}).fetchone()
-#         productPriceRange.append((id, prodPR[0], prodPR[1]))
-
-#     productTS_map = {
-#         int(pid): (title or 'No Title')
-#         for pid, title in productTS if pid is not None
-#     }
-#     productOptions_map = {int(pid): count for pid, count in productOptions if pid is not None}
-#     productPrice_map = {
-#         int(pid): (min_price, max_price) 
-#         for pid, min_price, max_price in productPriceRange if pid is not None
-#     }
-#     productVendor_map = {
-#         int(pid): username
-#         for pid, username in productVendor if pid is not None
-#     }
-#     prodCG_map = {
-#         int(pid): prod
-#         for pid, prod in prodCG if pid is not None
-#     }
-
-#     products = []
-#     for id_tuple in productIDs:
-#         pid = int(id_tuple[0])
-#         vendor = productVendor_map.get(pid, 'No vendor') 
-#         title = productTS_map.get(pid, ('No title'))
-#         options = productOptions_map.get(pid, 0)
-#         if pid in productPrice_map:
-#             min_price, max_price = productPrice_map[pid]
-#             price = toDollar(min_price) if min_price == max_price else f"{toDollar(min_price)} - {toDollar(max_price)}"
-#         else:
-#             price = "N/A"
-#         print(pid)
-#         category = prodCG_map.get(pid)
-#         print('category:', category)
-#         if pid != 850561 and pid != 850562:
-#             products.append({
-#                 'id': pid,
-#                 'title': title,
-#                 'options': options,
-#                 'price': price,
-#                 'vendor': vendor,
-#                 'category': category,
-#                 'display': True
-#             })
-#     vendors = conn.execute(
-#         text('SELECT username, CONCAT(first_name, " ", last_name) FROM users WHERE type = "vendor";')).fetchall()
-#     categorySO = conn.execute(
-#         text('SELECT cat_num, cat_name FROM categories ' \
-#         'WHERE cat_num BETWEEN 1 AND 99;')).fetchall()
-#     categorySC = conn.execute(
-#         text('SELECT cat_num, cat_name FROM categories ' \
-#         'WHERE cat_num BETWEEN 100 AND 199;')).fetchall()
-#     categoryOF = conn.execute(
-#         text('SELECT cat_num, cat_name FROM categories ' \
-#         'WHERE cat_num BETWEEN 200 AND 299;')).fetchall()
-#     categoryTX = conn.execute(
-#         text('SELECT cat_num, cat_name FROM categories ' \
-#         'WHERE cat_num BETWEEN 300 AND 399;')).fetchall()
-#     categoryTC = conn.execute(
-#         text('SELECT cat_num, cat_name FROM categories ' \
-#         'WHERE cat_num BETWEEN 400 AND 499;')).fetchall()
-#     categoryFT = conn.execute(
-#         text('SELECT cat_num, cat_name FROM categories ' \
-#         'WHERE cat_num BETWEEN 500 AND 599;')).fetchall()
-#     # print('category queries:', categoryFT, categoryOF, categorySC, categorySO, categoryTC, categoryTX)
-
-#     if request.method == 'POST':
-
-#         formSC = request.form.getlist('sc-options')
-#         formOF = request.form.getlist('of-options')
-#         formTC = request.form.getlist('tc-options')
-#         formTX = request.form.getlist('tx-options')
-#         formFT = request.form.getlist('ft-options')
-#         formCategories = [formSC, formOF, formTC, formTX, formFT,]
-#         print('******formCat:', formCategories)
-#         displayCategories(formCategories, products)
-
-#         formPrice = request.form.get('price')
-#         displayPrices(formPrice, products)
-
-#         formName = request.form.getlist('vendor-options') 
-#         displayVendors(formName, products)
-
-#         print('formname:', formName)
-#         formName_map = []
-
-#         for name in formName:
-#             for vendor in vendors:
-#                 if vendor[0] == name:
-#                     brand = vendor[1]
-#             print('Name', name, 'Brand:', brand)
-#             formName_map.append((name, brand))
-
-#         priceValue = toDollar(formPrice, html=True)
-#         clearDisplay = 'block'
-#         if formPrice is None:
-#             priceValue = 1000
-#             clearDisplay = 'none'
-
-#         return render_template(
-#             'search.html', 
-#             products = products,
-#             SO = categorySO,
-#             SC = categorySC,
-#             OF = categoryOF,
-#             TX = categoryTX,
-#             TC = categoryTC,
-#             FT = categoryFT,
-#             checkedVendors = formName_map,
-#             vendors = vendors, 
-#             priceValue = priceValue,
-#             clearDisplay = clearDisplay
-#         )
-    
-#     else:
-#         return render_template(
-#             'search.html', 
-#             products = products,
-#             SO = categorySO,
-#             SC = categorySC,
-#             OF = categoryOF,
-#             TX = categoryTX,
-#             TC = categoryTC,
-#             FT = categoryFT,
-#             vendors = vendors, 
-#             priceValue = 1000,
-#             clearDisplay = 'none'
-#         )
-    
 
 # -- FUNCTIONS -- #
 
