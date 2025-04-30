545a8b98433f70f16934b258f159ddf14e5b0579
bluemario8 <bluemario812@gmail.com>
2025-04-30 08:22:46 -0400

    Added some error checking for create product

--- product_manage/product_manage.py
+++ product_manage/product_manage.py
@@ -9,27 +9,49 @@ product_manage_bp = Blueprint("product_manage", __name__,
 
 
 @product_manage_bp.route("/manage", methods=["GET"])
+@product_manage_bp.route("/manage/<error>", methods=["GET"])
 @login_required
-def manage():
+def manage(error=None):
     if current_user.type == "customer":
         return redirect(url_for('login.login'))
 
-    
-    return render_template("product_manage.html", type=current_user.type)
+    return render_template("product_manage.html", type=current_user.type, error=error)
 
 
 @product_manage_bp.route("/manage/add", methods=["POST"])
 @login_required
 def create():
-    print(request.form)
+    error = None
     vendor_id = current_user.email if current_user.type == "vendor" else request.form.get("vendor_id")
     title = request.form.get("title")
     desc = request.form.get("description")
     warranty_months = request.form.get("warranty_months")
+
+    # error checks
+    vendorExists = conn.execute(text(f"SELECT email FROM users WHERE email = '{vendor_id}'")).first()
+    print(vendorExists)
+    if len(vendor_id) > 255 or not vendorExists:
+        error = "Invalid vendor email" 
+    elif len(title) > 255:
+        error = "Title is too long"
+    elif len(desc) > 500:
+        error = "Description is too long"
+    elif not warranty_months.isdigit() or int(warranty_months) > 2147483647 \
+        or int(warranty_months) < -2147483648:
+        error = "Invalid warranty"
+
+
+    if error:
+        return redirect(url_for("product_manage.manage", error=error))
+
     if warranty_months == 0 or warranty_months == "":   
         warranty_months = "NULL"
-    conn.execute(text("INSERT INTO products (vendor_id, product_title, "
-                      "product_description, warranty_months) " \
-                      f"VALUES ('{vendor_id}', '{title}', '{desc}', {warranty_months})"))
-    conn.commit()
+    try:
+        conn.execute(text("INSERT INTO products (vendor_id, product_title, "
+                        "product_description, warranty_months) " \
+                        f"VALUES ('{vendor_id}', '{title}', '{desc}', {warranty_months})"))
+        conn.commit()
+    except:
+        return redirect(url_for("product_manage.manage", error="Unknown error"))
+
     return redirect(url_for("product_manage.manage"))
