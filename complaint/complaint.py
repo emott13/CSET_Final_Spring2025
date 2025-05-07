d1f494efd88c66e3512e7ae804931e9246e1f1a1
bluemario8 <bluemario8@yahoo.com>
2025-05-07 16:12:40 -0400

    Admins can now view complaints and update their status (need to add data for which admin changed it)

--- complaint/complaint.py
+++ complaint/complaint.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
 from sqlalchemy import text
-from extensions import conn, dict_db_data
+from extensions import conn, dict_db_data, getCurrentType
 
 complaint_bp = Blueprint('complaint', __name__, static_folder='static_complaint', 
                          template_folder='templates_complaint', url_prefix="/complaint")
@@ -12,6 +12,46 @@ def complaint():
     complaints = dict_db_data('complaints', f"WHERE submitted_by = '{current_user.email}'")
     print(complaints)
     return render_template("complaint.html", complaints=complaints)
+
+@complaint_bp.route("/admin/", methods=["GET"])
+@login_required
+def admin():
+    if getCurrentType() != "admin":
+        return redirect(url_for("home.home"))
+
+    complaints = dict_db_data('complaints', "ORDER BY date DESC")
+    statuses = sql_enum_list(
+        conn.execute(text("SHOW COLUMNS FROM complaints LIKE 'status'")).all()[0][1])
+    error = request.args.get("error")
+
+    return render_template("admin.html", complaints=complaints, statuses=statuses, error=error)
+
+@complaint_bp.route("/admin/update/<int:complaintId>", methods=["POST"])
+@login_required
+def updateStatus(complaintId):
+    if getCurrentType() != "admin":
+        return redirect(url_for("home.home"))
+
+    statuses = sql_enum_list(
+        conn.execute(text("SHOW COLUMNS FROM complaints LIKE 'status'")).all()[0][1])
+    newStatus = request.form.get("status")
+    error = None
+
+    if newStatus not in statuses:
+        error = "Invalid status"
+    if error:
+        return redirect(url_for("complaint.admin", error=error))
+
+    try:
+        conn.execute(text("UPDATE complaints " +
+            "SET status = :newStatus WHERE complaint_id = :complaintId"),
+            {'newStatus': newStatus, 'complaintId': complaintId})
+        conn.commit()
+    except Exception as e:
+        print(f"\n {e} \n")
+        return redirect(url_for("complaint.admin", error="Unknown"))
+
+    return redirect(url_for("complaint.admin"))
 
 @complaint_bp.route('/create/<int:orderId>', methods=["GET", "POST"])
 @login_required
