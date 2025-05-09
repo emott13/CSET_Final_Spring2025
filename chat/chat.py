d239de1b6cf5f8c03c89c5fb5111ff5db5521518
bluemario8 <bluemario812@gmail.com>
2025-05-09 16:34:06 -0400

    Non-functioning chat home page work

--- chat/chat.py
+++ chat/chat.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
 from sqlalchemy import text
 from extensions import conn, dict_db_data, getCurrentType
+from ast import literal_eval
 
 chat_bp = Blueprint('chat', __name__, static_folder='static_chat', 
                          template_folder='templates_chat', url_prefix="/chat")
@@ -10,8 +11,47 @@ chat_bp = Blueprint('chat', __name__, static_folder='static_chat',
 @login_required
 def home():
     error = request.args.get("error")
+    chatsData = conn.execute(text("SELECT complaint_id, product_id, JSON_OBJECTAGG('user_from', " \
+        "user_from), JSON_OBJECTAGG('user_to', user_to) FROM chats " \
+        "WHERE (user_to = :email or user_from = :email )" \
+        "GROUP BY complaint_id, product_id"),
+        {'email': current_user.email}).all()
+    # list of dictionaries for each row
+    chats = []
+    for chat in chatsData:
+        chats.append({'complaint_id': chat[0], 'product_id': chat[1], 
+                      'user_from': literal_eval(chat[2])['user_from'], 
+                      'user_to': literal_eval(chat[3])['user_to']})
+    complaintIds = {}
+    productIds = {}
+    toEmailsComplaints = {}
+    for chat in chats:
+        if chat['complaint_id']:
+            complaints = dict_db_data("chats", 
+                f"WHERE complaint_id = {chat['complaint_id']} AND "
+                f"   (user_to = '{current_user.email}' OR user_from = '{current_user.email}' ) "\
+                "ORDER BY date_time") 
+            for chats in complaints:
+                other = chat['user_to'] if chat['user_to'] != current_user.email else chat['user_from']
+                print()
+                print(chat)
+                if other not in toEmailsComplaints.keys():
+                    toEmailsComplaints[other] = []
+                toEmailsComplaints[other].append(chats)
+            print(toEmailsComplaints)
+            # complaintIds[chat['complaint_id']]
+        elif chat['product_id']:
+            productIds[chat['product_id']] = dict_db_data("chats", 
+                f"WHERE product_id = {chat['product_id']} AND "
+                f"   (user_to = '{current_user.email}' OR user_from = '{current_user.email}' ) "\
+                "ORDER BY date_time")
+    # print(chats)
+    print(complaintIds)
+    # print(productIds)
 
-    return render_template('chat.html', error=error)
+
+    return render_template('chat.html', error=error, complaintIds=complaintIds, 
+                           productIds=productIds, toEmailsComplaints=toEmailsComplaints)
 
 @chat_bp.route("/<type>/<int:combined_id>/<string:recieverEmail>", 
                methods=["GET", "POST"])
